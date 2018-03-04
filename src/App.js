23c62f94eb9848696b7fc0881f0f84ef4ea70645
Raj <kmrrajj@gmail.com>
2018-03-04 16:34:08 +0530

    added player info

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import lottery from './lottery';
 
 class App extends Component {
   state = {
-    manager: ''
+    manager: '',
+    players: []
   };
   async componentDidMount(){
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers().call();
+    this.setState({ manager, players });
   }
   render() {
     //console.log(web3.version); // to make sure correct version of web3 has been installed.
@@ -20,7 +22,8 @@ class App extends Component {
           Lottery Contract
         </h2>
         <p>
-          This Contract is managed by {this.state.manager}
+          This Contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} players in the game.
         </p>
       </div>
     );
